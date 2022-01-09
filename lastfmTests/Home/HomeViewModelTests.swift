bbd2ef724f9d721f9536539eb7cebb41b290c14b
Nicolau Atala <nicolau.atala@hotmail.com>
2022-01-09 19:52:49 +0000

    feat(home) add test case to homeViewModel

--- lastfmTests/Home/HomeViewModelTests.swift
+++ lastfmTests/Home/HomeViewModelTests.swift
@@ -5,15 +5,33 @@ final class HomeViewModelTests: XCTestCase {
 
 	private var sut: HomeViewModel?
 	private var service: MockHomeService?
+	private var coordinatorDelegate: MockHomeCoordinator?
 
 	override func setUpWithError() throws {
 		service = MockHomeService()
-		sut = HomeViewModel(service: service!)
+		coordinatorDelegate = MockHomeCoordinator()
+		sut = HomeViewModel(service: service!, coordinatorDelegate: coordinatorDelegate!)
 	}
 
 	func testShouldgetTopAlbumsFromServe() {
 		sut?.getTopAlbums()
-		XCTAssertNotNil(sut?.topAlbums)
+		XCTAssertEqual(1, sut?.albums.currentValue.count)
 	}
 
+	func testShouldShowAlbumDetailView() {
+		sut?.getTopAlbums()
+		sut?.albumDetail(with: 0)
+		XCTAssertTrue(coordinatorDelegate!.presentedDetail)
+	}
+}
+
+
+class MockHomeCoordinator: Coordinator, HomeViewModelCoordinatorDelegate {
+	var presentedDetail = false
+
+	func albumDetail(_ with: Album) {
+		presentedDetail = true
+	}
+
+	func start() {}
 }
